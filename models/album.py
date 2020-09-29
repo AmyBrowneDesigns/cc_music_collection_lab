959e16f72721025adbce089d1e6746775b536929
AmyBrowneDesigns <amybrowne20@gmail.com>
2020-09-29 16:45:49 +0100

    initial commit for set up of folders, files

---
+++ models/album.py
@@ -0,0 +1,7 @@
+class Album:
+
+    def __init__(self, name, genre, artist_id )
+    self.name = name
+    self.genre = genre
+    self.artist_id = artist_id
+
---
+++ repositories/artist_repository.py
@@ -0,0 +1,7 @@
+from DB.run_sql import run_sql
+
+from models.album import Album
+from models.artist import Artist
+
+def save(album):
+    pass
